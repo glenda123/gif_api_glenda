d8b4d37b2893fe1a83d9cfa24c4549b6262cdeeb
Glenda Guerrero <glendaguerrerof@gmail.com>
2020-12-25 20:36:43 -0500

    subiendo cambios

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -24,6 +24,7 @@ const AddCategory = ({setCategories}) => {
                 <input type="text"
                     value={inputValue}
                     onChange={handleInputChange}
+                    placeholder='Ingresa el nombre del giff aquí'
 
                 />
             
